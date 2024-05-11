3c8f651b3f6556cbf31057aa2c1cfb34bd067c67
Nino Filiu <nino.filiu@gmail.com>
2024-05-11 22:19:59 +0200

    upload file

--- main.ts
+++ main.ts
@@ -6,8 +6,15 @@ Deno.serve({ port: 8080, hostname: address }, async (req) => {
   switch (`${req.method} ${url.pathname}`) {
     case "GET /":
     case "GET /index.html": {
-      const file = await Deno.open("./index.html");
-      return new Response(file.readable);
+      const page = await Deno.open("./index.html");
+      return new Response(page.readable);
+    }
+    case "POST /upload": {
+      const formData = await req.formData();
+      const file = formData.get("file") as File;
+      await Deno.writeFile(`./uploads/${file.name}`, file.stream());
+      const page = await Deno.open("./ok.html");
+      return new Response(page.readable);
     }
     default:
       return new Response(`Not found ${req.method} ${url.pathname}`);
